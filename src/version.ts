a2398e327e6b957270b47f1c37f3ece47ada67ae
Naylence Dev <212803464+naylencedev@users.noreply.github.com>
2025-12-01 17:42:34 -0800

    Updated dependencies

--- src/version.ts
+++ src/version.ts
@@ -1,8 +1,8 @@
 // This file is auto-generated during build - do not edit manually
-// Generated from package.json version: 0.3.4-test.716
+// Generated from package.json version: 0.3.4-test.731
 
 /**
  * The package version, injected at build time.
  * @internal
  */
-export const VERSION = '0.3.4-test.716';
+export const VERSION = '0.3.4-test.731';
